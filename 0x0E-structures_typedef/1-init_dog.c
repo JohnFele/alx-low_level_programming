b9e4cff1f59d85031df26c00a64ce57a89ae67db
JohnFele <fele476@gmail.com>
2023-06-20 00:54:51 +0200

    2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <dog.h>
+
+/**
+ * dog - struct data type
+ * @p: pointer to struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (p == NULL)
+		p = sizeof(struct dog);
+	p.name = name;
+	p.age = age;
+	p.owner = owner;
+}
