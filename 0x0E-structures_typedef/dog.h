69cc4b3bb3b55c7c6325fc8562f82d53cc209b06
JohnFele <fele476@gmail.com>
2023-06-20 00:42:13 +0200

    First

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog_h struct
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: New struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
