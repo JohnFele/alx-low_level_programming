29bd3bea5491c07c8400b8f753e6c7ce0c3fd7b0
JohnFele <fele476@gmail.com>
2023-06-19 23:44:43 +0200

    New

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef function-like_Macro_h
-#define function-like_Macro_h
+#ifndef function_like_Macro_h
+#define function_like_Macro_h
 
-#define ABS(x) ((x) < (0)? -(x) : (x))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
