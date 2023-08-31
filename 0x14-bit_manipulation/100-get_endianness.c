3c0bb1dce9595afc43a7fe24d034fec335ee4a28
JohnFele <fele476@gmail.com>
2023-09-01 01:59:41 +0200

    Advanced tasks

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * get_endianness - checks if a machine is little or big endian
-* 
-* Return: 0 for big, 1 for little
+*
+* Return: 0 for big endian, 1 for little endian
 */
 int get_endianness(void)
 {
