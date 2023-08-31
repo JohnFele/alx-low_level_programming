06a5ba0ff10efd266fda12c8b86f6bb88b7a774e
JohnFele <fele476@gmail.com>
2023-09-01 01:30:17 +0200

    Fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+	int i;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	for (int i = 0; b[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
-			result = 2 * result + (b[i] - '0');
-		else
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		result = 2 * result + (b[i] - '0');
 	}
 	return (result);
 }
